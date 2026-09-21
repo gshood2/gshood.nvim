4992fe44f7bbfc819eb8fea32fcc134971d3e430
gshood <gshood2@gmail.com>
2026-09-21 00:19:13 -0400

    custom plugins + theme

---
+++ lua/custom/plugins/custom.lua
@@ -0,0 +1,12 @@
+--My Additional plugins
+return{
+-- Auto pairs
+  {
+    'windwp/nvim-autopairs',
+    event = "InsertEnter",
+    opts = {} -- this is equalent to setup({}) function
+},
+
+-- EWW bar syntax support
+  {'elkowar/yuck.vim'}}
+
